1537461d446d75430f25c0b0ff67af17e7840915
DaleFu22 <116727779+DaleFu22@users.noreply.github.com>
2023-07-13 17:52:27 +0800

    Add files via upload

---
+++ C_program/8_day/multifile/include/calculator.h
@@ -0,0 +1,12 @@
+#ifndef __CALCULATOR_H 
+#define __CALCULATOR_H 
+
+#include <stdio.h>
+#include "../include/fun.h"
+
+int add_num(int a,int b);
+int sub_num(int a,int b);
+int mul_num(int a,int b);
+int div_num(int a,int b);
+
+#endif
---
+++ C_program/8_day/multifile/include/fun.h
@@ -0,0 +1,14 @@
+#ifndef __FUN_H
+#define __FUN_H
+
+#include <stdio.h>
+
+struct test{
+	int a;
+	char b;
+}; 
+
+void print_struct(void);
+
+#endif
+
